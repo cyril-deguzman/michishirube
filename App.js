bdf503a169d73362b9d81a10cae8543119918929
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2021-11-20 11:02:23 +0800

    added bottom nav

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function CameraScreen() {
+    return(
+        <View style={styles.homeScreen}>
+            <Text>
+                Camera
+            </Text>
+        </View>
+    );
+}
+
+function HomeScreen() {
+    return(
+        <View style={styles.homeScreen}>
+            <Text>
+                Home
+            </Text>
+        </View>
+    );
+}
+
+function BookmarkScreen() {
+    return(
+        <View style={styles.homeScreen}>
+            <Text>Bookmarks</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+        <Tab.Screen name="Camera" component={CameraScreen} />
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Bookmark" component={BookmarkScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  homeScreen: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
